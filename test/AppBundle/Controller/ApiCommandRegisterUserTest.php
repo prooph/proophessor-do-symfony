2b6e6685b56d375b52717ef23351b9c42e9afa72
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-01-11 00:39:56 -0300

    Separate tests by eventType

---
+++ test/AppBundle/Controller/ApiCommandRegisterUserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ProophessorTest\AppBundle\Controller;
+
+use Prooph\EventStore\Stream\StreamName;
+use Prooph\ProophessorDo\Model\Todo\Event\TodoWasPosted;
+use Prooph\ProophessorDo\Model\User\Event\UserWasRegistered;
+use Rhumsaa\Uuid\Uuid;
+
+class ApiCommandRegisterUserTest extends ControllerBaseTestCase
+{
+    public function test_command_register_user_returns_http_status_202()
+    {
+        $this->registerUser(Uuid::uuid4(), 'testUserName'.rand(10000, 1000000000), 'testUserEMail'.rand(10000, 1000000000).'@prooph.com');
+        $this->assertEquals(202, self::$client->getResponse()->getStatusCode());
+    }
+
+    public function test_command_register_user_adds_UserWasRegistered_event_to_eventstream()
+    {
+        $this->registerUser(Uuid::uuid4(), 'testUserName'.rand(10000, 1000000000), 'testUserEMail'.rand(10000, 1000000000).'@prooph.com');
+
+        $stream = $this->store->load(new StreamName('event'));
+        $this->assertCount(1, $stream->streamEvents());
+        $event = $stream->streamEvents()->current();
+        $this->assertInstanceOf(UserWasRegistered::class, $event);
+    }
+}
